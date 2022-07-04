bc5139c9663f4c42ca1fad0f37e8fab9d2e02778
ian <iansantosnascimento@gmail.com>
2022-07-04 00:05:51 -0400

    First Commit

---
+++ src/main/java/br/com/apirestfull/apigateway/exceptionHandler/CustomizedGlobalException.java
@@ -0,0 +1,36 @@
+package br.com.apirestfull.apigateway.exceptionHandler;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+/**
+ * Classe de exceção criada para quando algum controller lançar uma exceção não tratada, lançar esta
+ * exceção
+ */
+@RestController
+@ControllerAdvice
+public class CustomizedGlobalException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception e, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundException(Exception e,
+                                                                           WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
